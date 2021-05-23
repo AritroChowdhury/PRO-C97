ccecf9dffb504df8378ff0ac101697f351d26d23
AritroChowdhury <71551202+AritroChowdhury@users.noreply.github.com>
2021-05-23 13:30:46 +0530

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,11 @@
+introString=input('Enter The No:')
+charecterCount=0
+wordCount=1
+
+for i in introString:
+    charecterCount=charecterCount+1
+    if(i==' '):
+        noCount=noCount+1
+
+print(charecterCount)
+print(noCount)
